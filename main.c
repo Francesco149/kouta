571f927951bd8e1808c464ffbc915524702926e7
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-20 16:06:32 +0200

    opcode 0x44: ld b,h

--- main.c
+++ main.c
@@ -2174,7 +2174,7 @@ kt_op_t kt_op_table[512] = {
     { 0x41, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0x42, "LD", KT_REG, KT_B, KT_REG, KT_D, kt_ld_reg8_reg8, 1, 4 },
     { 0x43, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
-    { 0x44, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
+    { 0x44, "LD", KT_REG, KT_B, KT_REG, KT_H, kt_ld_reg8_reg8, 1, 4 },
     { 0x45, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0x46, "LD", KT_REG, KT_B, KT_REG_IND, KT_HL,
         kt_ld_reg8_reg16_ind, 1, 8 },
