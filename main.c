22a1dfb6e4065285a056120960ab53fdfb500bbb
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-20 21:24:24 +0200

    opcode 0xDA: jp c imm16

--- main.c
+++ main.c
@@ -1826,6 +1826,13 @@ void kt_jp_nz_imm16(kouta_t* kt, kt_op_t* instruction)
     kt_jp_cond_imm16(kt, KT_ZERO, 0, instruction);
 }
 
+/* jp c,imm16 */
+void kt_jp_c_imm16(kouta_t* kt, kt_op_t* instruction)
+{
+    (void)instruction;
+    kt_jp_cond_imm16(kt, KT_CARRY, KT_CARRY, instruction);
+}
+
 /* di */
 void kt_di(kouta_t* kt, kt_op_t* instruction)
 {
@@ -2504,7 +2511,7 @@ kt_op_t kt_op_table[512] = {
     { 0xD7, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0xD8, "RET C", 0, 0, 0, 0, kt_ret_c, 1, 8 },
     { 0xD9, "RETI", 0, 0, 0, 0, kt_reti, 1, 16 },
-    { 0xDA, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
+    { 0xDA, "JP C", KT_IMM16, 0, 0, 0, kt_jp_c_imm16, 1, 12 },
     { 0xDB, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0xDC, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0xDD, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
