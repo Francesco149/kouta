a5e645cdaa583aa57fff9aa39c7e49593eb13e24
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-20 22:27:08 +0200

    opcode 0x4ECB: bit 1,(hl)

--- main.c
+++ main.c
@@ -2678,7 +2678,7 @@ kt_op_t kt_op_table[512] = {
     { 0x4B, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x4C, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x4D, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
-    { 0x4E, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x4E, "BIT", KT_REG_IND, KT_HL, KT_BIT, 1, kt_bit_hl_ind, 2, 16 },
     { 0x4F, "BIT", KT_REG, KT_A, KT_BIT, 1, kt_bit_reg8, 2, 8 },
     { 0x50, "BIT", KT_REG, KT_B, KT_BIT, 2, kt_bit_reg8, 2, 8 },
     { 0x51, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
