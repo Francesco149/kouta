568c84b657213461e8225d86912b4643801a4d89
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-20 22:10:37 +0200

    fix wrong opcode size for 0xC2

--- main.c
+++ main.c
@@ -2488,7 +2488,7 @@ kt_op_t kt_op_table[512] = {
     { 0xBF, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0xC0, "RET NZ", 0, 0, 0, 0, kt_ret_nz, 1, 8 },
     { 0xC1, "POP", KT_REG, KT_BC, 0, 0, kt_pop_reg16, 1, 12 },
-    { 0xC2, "JP NZ", KT_IMM16, 0, 0, 0, kt_jp_nz_imm16, 1, 12 },
+    { 0xC2, "JP NZ", KT_IMM16, 0, 0, 0, kt_jp_nz_imm16, 3, 12 },
     { 0xC3, "JP", KT_IMM16, 0, 0, 0, kt_jp_imm16, 3, 16 },
     { 0xC4, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0xC5, "PUSH", KT_REG, KT_BC, 0, 0, kt_push_reg16, 1, 16 },
