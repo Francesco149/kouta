da8f5449864130fa5389a655b89b6fe02b0a3320
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:47:26 +0200

    opcode 0x88CB: res 1,b

--- main.c
+++ main.c
@@ -3115,7 +3115,7 @@ kt_op_t kt_op_table[512] = {
     { 0x85, "RES", KT_REG, KT_L, KT_BIT, 0, kt_res_reg8, 2, 8 },
     { 0x86, "RES", KT_REG_IND, KT_HL, KT_BIT, 0, kt_res_hl_ind, 2, 16 },
     { 0x87, "RES", KT_REG, KT_A, KT_BIT, 0, kt_res_reg8, 2, 8 },
-    { 0x88, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x88, "RES", KT_REG, KT_B, KT_BIT, 1, kt_res_reg8, 2, 8 },
     { 0x89, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x8A, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x8B, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
