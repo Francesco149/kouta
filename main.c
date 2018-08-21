227beada70e6578b812814224ea3c86870c872db
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:52:40 +0200

    opcode 0x9BCB: res 3,e

--- main.c
+++ main.c
@@ -3134,7 +3134,7 @@ kt_op_t kt_op_table[512] = {
     { 0x98, "RES", KT_REG, KT_B, KT_BIT, 3, kt_res_reg8, 2, 8 },
     { 0x99, "RES", KT_REG, KT_C, KT_BIT, 3, kt_res_reg8, 2, 8 },
     { 0x9A, "RES", KT_REG, KT_D, KT_BIT, 3, kt_res_reg8, 2, 8 },
-    { 0x9B, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x9B, "RES", KT_REG, KT_E, KT_BIT, 3, kt_res_reg8, 2, 8 },
     { 0x9C, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x9D, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x9E, "RES", KT_REG_IND, KT_HL, KT_BIT, 3, kt_res_hl_ind, 2, 16 },
