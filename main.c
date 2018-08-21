6b01a20717bfb5484c9e1840bedef7df0e37f84c
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 19:29:51 +0200

    opcode 0xEBCB: set 5,e

--- main.c
+++ main.c
@@ -3214,7 +3214,7 @@ kt_op_t kt_op_table[512] = {
     { 0xE8, "SET", KT_REG, KT_B, KT_BIT, 5, kt_set_reg8, 2, 8 },
     { 0xE9, "SET", KT_REG, KT_C, KT_BIT, 5, kt_set_reg8, 2, 8 },
     { 0xEA, "SET", KT_REG, KT_D, KT_BIT, 5, kt_set_reg8, 2, 8 },
-    { 0xEB, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0xEB, "SET", KT_REG, KT_E, KT_BIT, 5, kt_set_reg8, 2, 8 },
     { 0xEC, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0xED, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0xEE, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
