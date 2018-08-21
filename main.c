8b49342007589220c4b72e55ff6f7c6feeccf627
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 03:12:55 +0200

    opcode 0x6D: ld l,l

--- main.c
+++ main.c
@@ -2578,7 +2578,7 @@ kt_op_t kt_op_table[512] = {
     { 0x6A, "LD", KT_REG, KT_L, KT_REG, KT_D, kt_ld_reg8_reg8, 1, 4 },
     { 0x6B, "LD", KT_REG, KT_L, KT_REG, KT_E, kt_ld_reg8_reg8, 1, 4 },
     { 0x6C, "LD", KT_REG, KT_L, KT_REG, KT_H, kt_ld_reg8_reg8, 1, 4 },
-    { 0x6D, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
+    { 0x6D, "LD", KT_REG, KT_L, KT_REG, KT_L, kt_ld_reg8_reg8, 1, 4 },
     { 0x6E, "LD", KT_REG, KT_L, KT_REG_IND, KT_HL,
         kt_ld_reg8_reg16_ind, 1, 8 },
     { 0x6F, "LD", KT_REG, KT_L, KT_REG, KT_A, kt_ld_reg8_reg8, 1, 4 },
