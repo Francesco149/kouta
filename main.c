5d83af79eeb4375abbe655b3868738787271777e
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:11:20 +0200

    opcode 0x24CB: sla h

--- main.c
+++ main.c
@@ -2970,7 +2970,7 @@ kt_op_t kt_op_table[512] = {
     { 0x21, "SLA", KT_REG, KT_C, 0, 0, kt_sla_reg8, 2, 8 },
     { 0x22, "SLA", KT_REG, KT_D, 0, 0, kt_sla_reg8, 2, 8 },
     { 0x23, "SLA", KT_REG, KT_E, 0, 0, kt_sla_reg8, 2, 8 },
-    { 0x24, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x24, "SLA", KT_REG, KT_H, 0, 0, kt_sla_reg8, 2, 8 },
     { 0x25, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x26, "SLA", KT_REG_IND, KT_HL, 0, 0, kt_sla_hl_ind, 2, 16 },
     { 0x27, "SLA", KT_REG, KT_A, 0, 0, kt_sla_reg8, 2, 8 },
