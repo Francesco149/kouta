5c448059ba3f3b4463729b9804bfe608743cbb55
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 17:23:52 +0200

    opcode 0xB5: or l

--- main.c
+++ main.c
@@ -2748,7 +2748,7 @@ kt_op_t kt_op_table[512] = {
     { 0xB2, "OR", KT_REG, KT_D, 0, 0, kt_or_reg8, 1, 4 },
     { 0xB3, "OR", KT_REG, KT_E, 0, 0, kt_or_reg8, 1, 4 },
     { 0xB4, "OR", KT_REG, KT_H, 0, 0, kt_or_reg8, 1, 4 },
-    { 0xB5, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
+    { 0xB5, "OR", KT_REG, KT_L, 0, 0, kt_or_reg8, 1, 4 },
     { 0xB6, "OR", KT_REG_IND, KT_HL, 0, 0, kt_or_hl_ind, 1, 8 },
     { 0xB7, "OR", KT_REG, KT_A, 0, 0, kt_or_reg8, 1, 4 },
     { 0xB8, "CP", KT_REG, KT_B, 0, 0, kt_cp_reg8, 1, 4 },
