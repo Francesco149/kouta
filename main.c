53b76e3e58631575d857bc4195bee0d76b5e7b61
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:50:26 +0200

    opcode 0x91CB: res 2,c

--- main.c
+++ main.c
@@ -3124,7 +3124,7 @@ kt_op_t kt_op_table[512] = {
     { 0x8E, "RES", KT_REG_IND, KT_HL, KT_BIT, 1, kt_res_hl_ind, 2, 16 },
     { 0x8F, "RES", KT_REG, KT_A, KT_BIT, 1, kt_res_reg8, 2, 8 },
     { 0x90, "RES", KT_REG, KT_B, KT_BIT, 2, kt_res_reg8, 2, 8 },
-    { 0x91, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x91, "RES", KT_REG, KT_C, KT_BIT, 2, kt_res_reg8, 2, 8 },
     { 0x92, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x93, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x94, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
