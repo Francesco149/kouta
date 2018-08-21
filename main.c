c994e70f111f56c41e0c9128317fb5f9069e0888
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 19:29:16 +0200

    opcode 0xE8CB: set 5,b

--- main.c
+++ main.c
@@ -3211,7 +3211,7 @@ kt_op_t kt_op_table[512] = {
     { 0xE5, "SET", KT_REG, KT_L, KT_BIT, 4, kt_set_reg8, 2, 8 },
     { 0xE6, "SET", KT_REG_IND, KT_HL, KT_BIT, 4, kt_set_hl_ind, 2, 16 },
     { 0xE7, "SET", KT_REG, KT_A, KT_BIT, 4, kt_set_reg8, 2, 8 },
-    { 0xE8, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0xE8, "SET", KT_REG, KT_B, KT_BIT, 5, kt_set_reg8, 2, 8 },
     { 0xE9, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0xEA, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0xEB, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
