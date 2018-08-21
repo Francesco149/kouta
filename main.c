58e59202c0f68629bd92123ded0306bfb90cea4a
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 17:32:34 +0200

    opcode 0x9B: sbc a,e

--- main.c
+++ main.c
@@ -2722,7 +2722,7 @@ kt_op_t kt_op_table[512] = {
     { 0x98, "SBC", KT_REG, KT_A, KT_REG, KT_B, kt_sbc_a_reg8, 1, 4 },
     { 0x99, "SBC", KT_REG, KT_A, KT_REG, KT_C, kt_sbc_a_reg8, 1, 4 },
     { 0x9A, "SBC", KT_REG, KT_A, KT_REG, KT_D, kt_sbc_a_reg8, 1, 4 },
-    { 0x9B, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
+    { 0x9B, "SBC", KT_REG, KT_A, KT_REG, KT_E, kt_sbc_a_reg8, 1, 4 },
     { 0x9C, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0x9D, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0x9E, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
