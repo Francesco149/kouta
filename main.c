241c7d5d78820d8bb3e161224c89a5f3f948d8ba
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 19:08:30 +0200

    opcode 0xB4CB: res 6,h

--- main.c
+++ main.c
@@ -3159,7 +3159,7 @@ kt_op_t kt_op_table[512] = {
     { 0xB1, "RES", KT_REG, KT_C, KT_BIT, 6, kt_res_reg8, 2, 8 },
     { 0xB2, "RES", KT_REG, KT_D, KT_BIT, 6, kt_res_reg8, 2, 8 },
     { 0xB3, "RES", KT_REG, KT_E, KT_BIT, 6, kt_res_reg8, 2, 8 },
-    { 0xB4, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0xB4, "RES", KT_REG, KT_H, KT_BIT, 6, kt_res_reg8, 2, 8 },
     { 0xB5, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0xB6, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0xB7, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
