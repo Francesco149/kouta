435284fe1ec862e4016de1c37d49f09c45cafed1
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:31:06 +0200

    opcode 0x51CB: bit 2,c

--- main.c
+++ main.c
@@ -3060,7 +3060,7 @@ kt_op_t kt_op_table[512] = {
     { 0x4E, "BIT", KT_REG_IND, KT_HL, KT_BIT, 1, kt_bit_hl_ind, 2, 16 },
     { 0x4F, "BIT", KT_REG, KT_A, KT_BIT, 1, kt_bit_reg8, 2, 8 },
     { 0x50, "BIT", KT_REG, KT_B, KT_BIT, 2, kt_bit_reg8, 2, 8 },
-    { 0x51, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x51, "BIT", KT_REG, KT_C, KT_BIT, 2, kt_bit_reg8, 2, 8 },
     { 0x52, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x53, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x54, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
