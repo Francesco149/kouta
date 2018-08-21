9fca157276446b9249fc3099c1ae7a8ebe1e917a
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:37:42 +0200

    opcode 0x6ACB: bit 5,d

--- main.c
+++ main.c
@@ -3085,7 +3085,7 @@ kt_op_t kt_op_table[512] = {
     { 0x67, "BIT", KT_REG, KT_A, KT_BIT, 4, kt_bit_reg8, 2, 8 },
     { 0x68, "BIT", KT_REG, KT_B, KT_BIT, 5, kt_bit_reg8, 2, 8 },
     { 0x69, "BIT", KT_REG, KT_C, KT_BIT, 5, kt_bit_reg8, 2, 8 },
-    { 0x6A, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x6A, "BIT", KT_REG, KT_D, KT_BIT, 5, kt_bit_reg8, 2, 8 },
     { 0x6B, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x6C, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x6D, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
