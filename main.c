2caa187a6e1902382b61554abbe7da8e81e84d68
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-21 18:39:28 +0200

    opcode 0x74CB: bit 6,h

--- main.c
+++ main.c
@@ -3095,7 +3095,7 @@ kt_op_t kt_op_table[512] = {
     { 0x71, "BIT", KT_REG, KT_C, KT_BIT, 6, kt_bit_reg8, 2, 8 },
     { 0x72, "BIT", KT_REG, KT_D, KT_BIT, 6, kt_bit_reg8, 2, 8 },
     { 0x73, "BIT", KT_REG, KT_E, KT_BIT, 6, kt_bit_reg8, 2, 8 },
-    { 0x74, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
+    { 0x74, "BIT", KT_REG, KT_H, KT_BIT, 6, kt_bit_reg8, 2, 8 },
     { 0x75, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 2, 0 },
     { 0x76, "BIT", KT_REG_IND, KT_HL, KT_BIT, 6, kt_bit_hl_ind, 2, 16 },
     { 0x77, "BIT", KT_REG, KT_A, KT_BIT, 6, kt_bit_reg8, 2, 8 },
