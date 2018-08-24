37a7c30df4d717426e4f5eb93a232eed5db6013a
Franc[e]sco <lolisamurai@tfwno.gf>
2018-08-24 12:37:01 +0200

    fix incorrect instruction size for 0xDC: call c imm16

--- main.c
+++ main.c
@@ -2951,7 +2951,7 @@ kt_op_t kt_op_table[512] = {
     { 0xD9, "RETI", 0, 0, 0, 0, kt_reti, 1, 16 },
     { 0xDA, "JP C", KT_IMM16, 0, 0, 0, kt_jp_c_imm16, 3, 12 },
     { 0xDB, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
-    { 0xDC, "CALL C", KT_IMM16, 0, 0, 0, kt_call_c_imm16, 1, 12 },
+    { 0xDC, "CALL C", KT_IMM16, 0, 0, 0, kt_call_c_imm16, 3, 12 },
     { 0xDD, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
     { 0xDE, "SBC", KT_REG, KT_A, KT_IMM8, 0, kt_sbc_a_imm8, 2, 8 },
     { 0xDF, "UNIMPLEMENTED", 0, 0, 0, 0, kt_unimplemented, 1, 0 },
